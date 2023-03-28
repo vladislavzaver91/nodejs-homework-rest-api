6f6a2c6de9ca2d7cf6fee9bafc712f04fba431b9
Vladislav Zavertaylo <102727549+vladislavzaver91@users.noreply.github.com>
2023-03-28 18:56:27 +0300

    Adds start files to HW02

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,41 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises')
+const path = require('path');
+const {nanoid} = require('nanoid');
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, 'contacts.json');
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+}
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const result = contacts.find(contact => contact.id === contactId);
+  return result || null;
+}
 
-const addContact = async (body) => {}
+const addContact = async (data) => {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    ...data,
+  }
+  contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return newContact;
+};
 
-const updateContact = async (contactId, body) => {}
+// const removeContact = async (contactId) => {}
+
+// const addContact = async (body) => {}
+
+// const updateContact = async (contactId, body) => {}
 
 module.exports = {
   listContacts,
   getContactById,
-  removeContact,
   addContact,
-  updateContact,
+  // removeContact,
+  // updateContact,
 }
