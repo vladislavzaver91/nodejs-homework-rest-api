7670dabdf987c4965af0220a16f5e04e81f2d34c
Vladislav Zavertaylo <102727549+vladislavzaver91@users.noreply.github.com>
2023-04-30 18:25:47 +0300

    Fixed filter

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -5,15 +5,18 @@ const listContacts = async (req, res) => {
     const { _id: owner } = req.user;
     const { page = 1, limit = 20, favorite = '' } = req.query;
     const skip = (page - 1) * limit;
-    if (favorite === 'true' || favorite === 'false') {
-        const result = await Contact.find({ owner, favorite }, "-createdAt -updatedAt", { skip, limit }).populate("owner", "email subscription");
+    if (favorite === 'true') {
+        const result = await Contact.find({ owner, favorite: { $eq: true } }, "-createdAt -updatedAt", { skip, limit }).populate("owner", "email subscription");
+        res.json(result);
+    } if (favorite === 'false') {
+        const result = await Contact.find({ owner, favorite: { $eq: false } }, "-createdAt -updatedAt", { skip, limit }).populate("owner", "email subscription");
         res.json(result);
     } else {
         const result = await Contact.find({ owner }, "-createdAt -updatedAt", { skip, limit }).populate("owner", "email subscription");
-        if (!result) {
-            throw HttpError(404, "Not fount");
-        }
-        res.json(result)
+    if (!result) {
+        throw HttpError(404, "Not fount");
+    }
+    res.json(result);
     };
 };
 
