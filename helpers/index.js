21492055e925eba035c10859f830969940ede46c
Vladislav Zavertaylo <102727549+vladislavzaver91@users.noreply.github.com>
2023-05-11 18:30:33 +0300

    Finished HW06

--- helpers/index.js
+++ helpers/index.js
@@ -1,9 +1,11 @@
 const HttpError = require('./HttpError');
 const handleMongooseError = require('./handleMongooseError');
 const controllerWrapper = require('./controllerWrapper');
+const sendEmail = require('./sendEmail');
 
 module.exports = {
     HttpError,
     handleMongooseError,
     controllerWrapper,
+    sendEmail,
 }
